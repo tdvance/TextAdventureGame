9230b6b6544dea0c0f27b47a94d9eff44684a096
Todd D. Vance <tdvance@gmail.com>
2016-07-29 12:22:14 -0400

    Wrote most of methods for Action---still needs "run"

--- action.py
+++ action.py
@@ -14,7 +14,7 @@ class Action(GameObject):
     def __init__(self, name, desc=None, brief_desc=None):
         GameObject.__init__(self, self.__class__, name, desc, brief_desc)
         self._object = None
-        self._indirect_objects = []  # list of (Preposition, Noun)
+        self._indirect_objects = set()  # set of (Preposition, Noun)
 
     def set_object(self, item):
         self._object = GameObject.get(item)
@@ -26,25 +26,43 @@ class Action(GameObject):
         return self._object is not None
 
     def get_indirect_object(self, item):
-        pass
+        item = GameObject.get(item)
+        for (p, i) in self._indirect_objects:
+            if i == item:
+                return i
+        return None
 
     def get_preposition(self, item):
-        pass
+        item = GameObject.get(item)
+        for (p, i) in self._indirect_objects:
+            if i == item:
+                return p
+        return None
 
     def has_indirect_object(self, item):
-        pass
+        item = GameObject.get(item)
+        for (p, i) in self._indirect_objects:
+            if i == item:
+                return True
+        return False
 
     def num_indirect_objects(self):
-        pass
+        return len(self._indirect_objects)
 
     def iter_indirect_objects(self):
-        pass
+        return iter(self._indirect_objects)
 
     def add_indirect_object(self, preposition, item):
-        pass
+        item = GameObject.get(item)
+        preposition = GameObject.get(preposition)
+        self._indirect_objects.add((preposition, item))
 
     def remove_indirect_object(self, item):
-        pass
+        item = GameObject.get(item)
+        for (p, i) in self._indirect_objects:
+            if i == item:
+                self._indirect_objects.remove((p, i))
+                break
 
     def run(self):
         pass
