d26060480a766b6cf4f25aad442b3c42fe8b7b7c
Todd D. Vance <tdvance@gmail.com>
2016-08-01 07:51:45 -0400

    Added some testing code

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import csv
 from collections import namedtuple
+from collections import deque
 import random
 import sys
 
@@ -7,13 +8,22 @@ import resources
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, *queue):
+        """
+        Create and run a new game.
+
+        :param queue: commands to inject into input for testing
+        """
         self._starting_room = None
         self._room = None
         self._time = 0.0
         self._resources = resources.Resources()
         self.load_game()
         self._debug = False
+        self._test_input_queue = deque()
+        for s in queue:
+            self._test_input_queue.appendleft(str(s))  # for testing
+        self._transcript = []
 
     def load_game(self):
         self._starting_room = self._resources.load_resource('Room')
@@ -36,13 +46,18 @@ class Game:
         self._time += 1
         return result
 
+    def output(self, fmt, *args):
+        x = fmt % args
+        self._transcript.append(x)
+        print(x)
+
     def output_sentence(self, fmt, *args):
         x = fmt % args
         if x.endswith('.'):
             x = x[0].upper() + x[1:]
         else:
             x = x[0].upper() + x[1:] + '.'
-        print(x)
+        self.output('%s', x)
 
     def debug(self, fmt, *args):
         if self._debug:
@@ -69,21 +84,30 @@ class Game:
             self.output_sentence("%s is %s", random.choice(direction[2:l]), where.ShortDescription)
 
     def get_input(self):
-        s = input('> ')
+        if self._test_input_queue:
+            s = self._test_input_queue.pop()
+            print('> %s' % s)
+        else:
+            s = input('> ')
+        self._transcript.append('> %s' % s)
         return s.lower().split()
 
     def process_input(self, l):
         if not l:
-            return
+            return 'Enter a command.'
         cmd = l[0]
         if len(l) == 1 and self._resources.has(cmd, 'Direction'):
             return self.go(cmd)
-        cmd, category = self._resources.normalize(cmd, 'Command')
-        if cmd == 'help':
+        r = self._resources.get(cmd, 'Command')
+        if r is None:
+            self.output_sentence("I am not familiar with the command '%s'", cmd)
+            return "I don't understand"
+        cmd = r.Name
+        if cmd == 'Help':
             return self.help(*l[1:])
-        if cmd == 'quit':
+        if cmd == 'Quit':
             return False
-        if cmd == 'go':
+        if cmd == 'Go':
             return self.go(*l[1:])
 
         self.output_sentence("I don't understand")
@@ -128,5 +152,21 @@ class Game:
 
 
 if __name__ == '__main__':
-    game = Game()
+    game = Game('d', 'w', 'e', 's', 'go w', 'e', 'n', 'south', 'North', 'nw', 'h nw', 'q')
     game.start_game()
+    t = game._transcript
+    assert 'top of a roof' in t[0]
+    assert 'street below a tall building' in t[3]
+    assert 'apartment building' in t[7]
+    assert t[10] == t[3]
+    assert 'decrepit theater' in t[14]
+    assert 'front of a theater' in t[18]
+    assert t[21] == t[14]
+    assert t[25] == t[3]
+    assert t[29] == t[14]
+    assert t[33] == t[3]
+    assert 'nothing to see' in t[37]
+    assert 'Go Northwest' in t[42]
+    assert t[43] == t[3]
+    assert 'Game Over' in t[47]
+    print("Tests passed.")
